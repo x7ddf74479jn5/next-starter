eff06fc8bded1816a032d87b6fb7ec7051bd9526
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-15 19:19:28 +0900

    ✨ add: error boundary

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,15 @@
 import type { ReactNode, VFC } from "react";
 import { Footer } from "src/layout/Footer";
 import { Header } from "src/layout/Header";
+import { LayoutErrorBoundary } from "src/layout/LayoutErrorBoundary";
 
 export const Layout: VFC<{ children: ReactNode }> = (props) => {
   return (
     <>
       <Header />
-      <main>{props.children}</main>
+      <LayoutErrorBoundary>
+        <main>{props.children}</main>
+      </LayoutErrorBoundary>
       <Footer />
     </>
   );
