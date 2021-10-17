2844204b096074cf8854758e82aeea6b52dd1fb5
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-10-17 22:46:46 +0900

    🩹 chore

--- src/types/next.d.ts
+++ src/types/next.d.ts
@@ -2,13 +2,11 @@ import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 
-declare module "next" {
+declare module "next/app" {
   type NextPageWithLayout = NextPage & {
     getLayout?: (page: ReactElement) => ReactNode;
   };
-}
 
-declare module "next/app" {
   type AppPropsWithLayout = AppProps & {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     Component: NextPageWithLayout;
