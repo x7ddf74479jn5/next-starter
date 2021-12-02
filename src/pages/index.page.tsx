04d6a1ee151a4013497ed678df1be6a65cd84d77
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-02 19:21:02 +0900

    ✨ feat: PrimaryButton

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { PrimaryButton } from "src/components/atoms/buttons";
 import { Layout } from "src/layout/Layout";
 
 const Home: NextPage = () => {
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h2>Home</h2>
-      <button onClick={handleClick}>Button</button>
+      <PrimaryButton onClick={handleClick}>Button</PrimaryButton>
     </Layout>
   );
 };
