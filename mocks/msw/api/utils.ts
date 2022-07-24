6bb0e7d606925bedf2e6aa0de3d66e08d54341c3
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-07-24 16:57:59 +0900

    🩹 chore: msw

--- mocks/msw/api/utils.ts
+++ mocks/msw/api/utils.ts
@@ -1,9 +1,9 @@
-import type { DefaultRequestBody, RestRequest } from "msw";
+import type { DefaultBodyType, RestRequest } from "msw";
 
 const PORT = "8080";
 export const API_ROUTE = `https://localhost:${PORT}/api`;
 
-export const getSearchParams = (req: RestRequest<DefaultRequestBody>) => {
+export const getSearchParams = (req: RestRequest<DefaultBodyType>) => {
   const hoge = req.headers.get("hoge");
   return { hoge };
 };
